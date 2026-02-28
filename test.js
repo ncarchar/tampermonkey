83e22a34e377659fed69120293810aee37b761da
Carson Miller <carsoncmiller@proton.me>
2026-02-28 08:15:01 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         _Local Loader
+// @version      0.0.1
+// @match        https://www.youtube.com/*
+// @grant        none
+// @run-at       document-idle
+// @updateURL    https://raw.githubusercontent.com/ncarchar/tampermonkey/refs/heads/main/test.js
+// @downloadURL  https://raw.githubusercontent.com/ncarchar/tampermonkey/refs/heads/main/test.js
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    console.log("here");
+})();
